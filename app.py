c84d5cd3906bd5cfca67367228af7841f199a011
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2019-02-20 16:02:07 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, json, jsonify
+import requests
+import os
+app = Flask(__name__)
+
+
+# bikin kuis baru
+@app.route('/quiz', methods=['POST'])
+def createQuiz():
+    body = json.dumps(request.json)
+
+    quizData = {
+        "totalQuizAvailable": 0,
+        "quizzes": []
+    }
+
+    if os.path.exists('./quizzes-file.json'):
+        quizzesFile = open('./quizzes-file.json', 'r')
+        quizData = json.load(quizzesFile)
+    else:
+        quizzesFile = open('./quizzes-file.json', 'x')
+
+    quizData["totalQuizAvailable"] += 1
+    quizData["quizzes"].append(body)
+
+    quizzesFile = open('./quizzes-file.json', 'w')
+    quizzesFile.write(str(json.dumps(quizData)))
+
+    return str(quizData)
+
+# bikin soal untuk kuis yang udah ada
+@app.route('/question', methods=['POST'])
+def createQuestion():
+    body = json.dumps(request.json)
+
+    questionData = {
+        "questions": []
+    }
+
+    if os.path.exists('./question-file.json'):
+        questionFile = open('./question-file.json', 'r')
+        print("File ada")
+        questionData = json.load(questionFile)
+    else:
+        questionFile = open('./question-file.json', 'x')
+        print("file ga ada") 
+
+    questionFile = open('./question-file.json', 'w')
+    questionData["questions"].append(body)
+    questionFile.write(str(json.dumps(questionData)))
+
+    return str(questionData)
+
+# meminta data kuis dan soalnya
+@app.route('/quizzes/<quizId>')
+def getQuiz(quizId):
+    # nyari quiznya
+    quizzesFile = open('./quizzes-file.json')
+    quizzesData = json.load(quizzesFile)
+
+    for quiz in quizzesData["quizzes"]:
+        quiz = json.loads(quiz)
+        if quiz["quiz-id"] == int(quizId):
+            quizData = quiz
+            break
+
+    # nyari soalnya
+    questionsFile = open('./question-file.json')
+    questionsData = json.load(questionsFile)
+
+    for question in questionsData["questions"]:
+        question = json.loads(question)
+        if question["quiz-id"] == int(quizId):
+            quizData["question-list"].append(question)
+
+    return jsonify(quizData)
+
+# minta data sebuah soal untuk kuis tertentu
+@app.route('/quizzes/<quizId>/questions/<questionNumber>')
+def getThatQuestion(quizId, questionNumber):
+    quizData = getQuiz(int(quizId)).json
+
+    for question in quizData["question-list"]:
+        if question["question-number"] == int(questionNumber):
+            return jsonify(question)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=14045)
---
+++ sampah.py
@@ -0,0 +1,94 @@
+@app.route('/hikmat')
+def hikmat():
+    return ("waduh", "gitu")
+
+@app.route('/summation/<firstNum>/<secondNum>')
+def summation(firstNum, secondNum):
+    firstNum = int(firstNum)
+    secondNum = int(secondNum)
+    result = firstNum + secondNum
+    # saya yang bernama xxx mempunyai nilai yyy
+    return str(result)
+
+@app.route('/summation')
+def summation2():
+    firstNum = request.args.get('firstNum')
+    secondNum = request.args.get('secondNum')
+    result = firstNum + secondNum
+    
+    return str(result)
+
+@app.route('/')
+def home():
+    return "Mobil baru"
+
+
+historyUser = []
+
+@app.route('/codewars/history')
+def getSearchHistory():
+    historyFile = open("history-user", 'r') 
+    return historyFile.read()
+
+@app.route('/codewars/<username>')
+def getUserInfo(username):
+    historyFile = open("history-user", "a")
+    historyFile.write(username)
+    
+    historyUser.append(username)
+
+    data = requests.get("https://www.codewars.com/api/v1/users/%s" % username)
+    theName = data.json()["name"]
+    theHonor = data.json()["honor"]
+
+    result = "saya yang bernama %s mempunyai nilai %s" % (theName, theHonor)
+    return result
+
+@app.route('/questions/<questionNumber>')
+def getQuestion(questionNumber):
+    questionFile = open('./question-file.json')
+    questionData = json.load(questionFile)
+    print(type(questionData))
+
+    for question in questionData["questions"]:
+        # loads itu, dari string ke json
+        # dumps itu, dari singlequotes ke doublequotes
+        question = json.loads(question)
+        if question["question-number"] == int(questionNumber):
+            return str(question)
+
+    return "gaketemu soalnya"
+
+
+# ```
+# 1. ngebaca dokumentasi api codewars
+# 2. nyobain si api di insomnia/postman
+# 3. nginstall si `requests` di si flask
+# 4. kita pake si `requests` buat minta data dari api codewars
+# 5. si yang 4, kita mintanya di rute tertentu. bentuknya dinamis karena pake <params>
+# 6. kita bikin string dari beberapa data dari response api-nya codewars
+# ====iklan: nyobain port random, sama ip 0.0.0.0, via `app.run()`
+# 7. kita nyobain bikin variabel dan membuktikan itu temporary alias ilang pas servernya di restart: e.g. nyimpen di array
+# 8. supaya ga ilang, kita nyoba simpen di sebuah file
+# 9. baca filenya
+# 10. segitu
+# ```
+
+# ```
+# 1. kita nyobain POST
+# ```
+
+@app.route('/ngebuktiin', methods=['POST'])
+def ngebuktiin():
+    body = request.json
+    print(body)
+    print(json.dumps(body))
+
+    return "beres"
+
+
+# ```
+# 1. Pertama, kita bikin grup nya
+#     caranya mirip sama yang question
+# 2. bagian add question, yang bawah itu 
+# ```
